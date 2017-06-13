4d7845b7855f9790c1029ca10a11e7e60a4af31a
nathanHS <test@q.com>
2017-06-13 22:24:43 +0800

    添加 vector

--- main.cpp
+++ main.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
-#include "Sales_data.h"
-
 
 int main() {
 
-//	下面的写法更好
-//	struct Sales_data{
-//		std::string bookNo;
-//		unsigned units_sold = 0;
-//		double revenue = 0.0;
-//	}accum, trans,*salesptr;
+	unsigned cnt = 42;
+	constexpr unsigned sz = 42;
+
+//	非常量表达式定义数组
+//	int bad[cnt];
+//	int a = bad[cnt+1];
+//	bad[1] = 2;
+//	std::cout << bad[1];
+
+//	因为空字符长度不够
+//	char test[2] = "23";
+
+	char test[2] = {'2', '3'};
 
 
-//	Sales_data *salesptr;
-//	Sales_data accum{"abc",1,11.5};
-//	Sales_data trans = {"cc",2,42};
+	int arr[10];
 
-
-	Sales_data data1,data2;
-
-	double price = 0;
-//	读入第一笔交易的书号，数量，单价
-	std::cin >> data1.bookNo >> data1.units_sold >> price;
-	data1.revenue = data1.units_sold * price;
-
-	std::cin >> data2.bookNo >> data2.units_sold >> price;
-	data2.revenue = data2.units_sold * price;
-
-
+	int (*p)[10] = &arr;
+	int (*&d)[10] = p;
 
 	std::cout << "Hello, World!" << std::endl;
 
