e06c088b7809522163ef0f0470beb1f58fd573c4
nathanHS <test@q.com>
2017-06-17 15:46:50 +0800

    class

--- main.cpp
+++ main.cpp
@@ -1,28 +1,116 @@
 #include <iostream>
 
+int max() {
+	int i;
+	std::cin >> i;
+	return i;
+}
+
+class test {
+public:
+	int a = 1;
+	double b;
+	bool c;
+	int d;
+
+	test() = default;
+
+	test(int b) { this->b = b; }
+};
+
+class X {
+	int i = 200;
+	int j = 2000;
+public:
+//	未定义的：i 在 j 之前初始化
+	X(int val) : j(val), i(j) {};
+};
+
+class J {
+	int a = 2222;
+public:
+	J() {}
+};
+
+
+class Z {
+	int a = 1;
+	int b = 2;
+	int c = 3;
+public:
+	Z(int s, int d) : a(s), b(d) {
+		a = 1;
+		b = 2;
+	}
+
+	Z() : Z(0, 0) {
+		a = 2;
+		b = 3;
+	}
+
+};
+
+
+class aa {
+	int a;
+public:
+	aa(int aaa) : a(aaa) {
+		a = 22;
+	}
+
+	void function(aa aa1) {
+		a = aa1.a;
+	}
+};
+
+class bb {
+	aa tete;
+public:
+	bb() : tete(1) {}
+};
+
+class dd {
+public:
+	int a;
+	int b;
+	int c;
+};
+
+class ee {
+public:
+	int function(int a = max()) { return a; }
+
+	int a = max();
+
+};
+
+class gg {
+public:
+	static int ikk = 0;
+};
+
 int main() {
+	test a;
+	test b(11);
+	//const test c;
+	//c.a = 1;
+	std::cout << a.a;
 
-	unsigned cnt = 42;
-	constexpr unsigned sz = 42;
+	X aa(1);
+	J as;
+	Z ass;
 
-//	非常量表达式定义数组
-//	int bad[cnt];
-//	int a = bad[cnt+1];
-//	bad[1] = 2;
-//	std::cout << bad[1];
+	bb ad;
 
-//	因为空字符长度不够
-//	char test[2] = "23";
+	class aa ss(2);
+	ss.function(2);
 
-	char test[2] = {'2', '3'};
+	class aa ssd = 2;
+	//test b(2);
+//	dd sscs = {1,2,3};
+//	ee sssa;
+//	ee ss2sa;
+//	ss2sa.function();
 
-
-	int arr[10];
-
-	int (*p)[10] = &arr;
-	int (*&d)[10] = p;
-
-	std::cout << "Hello, World!" << std::endl;
-
-	return 0;
+	return 1;
 }
