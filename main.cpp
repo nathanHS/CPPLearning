4f198dbfb9670bae27ab69423745a0cb3f834b81
nathanHS <test@q.com>
2017-06-21 00:24:19 +0800

    iostream

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
 int main() {
 
-	unsigned cnt = 42;
-	constexpr unsigned sz = 42;
+	string a = "123";
+	string b = "345";
 
-//	非常量表达式定义数组
-//	int bad[cnt];
-//	int a = bad[cnt+1];
-//	bad[1] = 2;
-//	std::cout << bad[1];
-
-//	因为空字符长度不够
-//	char test[2] = "23";
-
-	char test[2] = {'2', '3'};
+	vector<int> ss(2, 1);
+	int gg = 23;
+	ss.emplace_back(gg);
 
 
-	int arr[10];
+	ss.emplace(ss.begin(), 22121);
+	++gg;
+	auto &c = b;
 
-	int (*p)[10] = &arr;
-	int (*&d)[10] = p;
+	swap(a, b);
 
-	std::cout << "Hello, World!" << std::endl;
+	int k = 1;
+
+	auto g = ss.size();
 
 	return 0;
 }
