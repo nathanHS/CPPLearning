8c98acd5216d2175967316eafd711b76cf0d9e9a
nathanHS <test@q.com>
2017-06-24 15:37:20 +0800

    Dynamic Memory

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 
+using namespace std;
+
+class a {
+private:
+public:
+	int kk = 1;
+
+	a() {
+		cout << "默认构造函数" << endl;
+	}
+
+	a(int k) : kk(k) { cout << "一个参数的构造函数" << endl; }
+
+	// 注释即默认的拷贝构造
+	//a(const a& tem){ cout << "引用拷贝构造函数" << endl;}
+
+};
+
+class b {
+	b(const b &) = delete;
+
+	b &operator=(const b &) = delete;
+};
+
 int main() {
 
-	unsigned cnt = 42;
-	constexpr unsigned sz = 42;
 
-//	非常量表达式定义数组
-//	int bad[cnt];
-//	int a = bad[cnt+1];
-//	bad[1] = 2;
-//	std::cout << bad[1];
+	a b;
+//	直接构造
+	a c(22);
+	a d(c); // 但是这个我觉得也是拷贝构造
 
-//	因为空字符长度不够
-//	char test[2] = "23";
+//	拷贝构造
+	a g = c;
+	a f = 12;
+	a h = {13};
+	a i = {a(232)};
 
-	char test[2] = {'2', '3'};
-
-
-	int arr[10];
-
-	int (*p)[10] = &arr;
-	int (*&d)[10] = p;
-
-	std::cout << "Hello, World!" << std::endl;
-
+	int a = 1;
+	int &&gs = a + 1;
 	return 0;
 }
